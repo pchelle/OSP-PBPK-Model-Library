f353610ea4c55cf16cfe7b98901afadd8a5dd711
Pierre Chelle <45343665+pchelle@users.noreply.github.com>
2024-11-06 03:48:24 -0500

    Update tools installation

--- install_tools.R
+++ install_tools.R
@@ -8,14 +8,39 @@
 install.packages(c("dplyr", "purrr", "covr", "readr", "tidyr", "webshot", "spelling", "readxl", "data.table", "gridtext", "ggtext", "tidyselect", "testthat", "rmarkdown", "rsvg", "svglite", "cowplot"), repos = "http://cran.us.r-project.org", type = "win.binary")
 
 toolsData <- read.csv("tools.csv", stringsAsFactors = FALSE, colClasses = "character")
+print(toolsData)
 
-#' @title getToolPath
-getToolPath <- function(toolName) {
+#' @title installTool
+#' @description
+#' Install tool by tool name and version
+#' TODO: this may be changed later on because newer versions of OSPSuite may rely on `{remotes}` package directly
+#' @param toolName Identifier of the tool to install
+#' @param toolsData data.frame mapping tool identifier and version
+installTool <- function(toolName, toolsData = toolsData) {
   toolVersion <- toolsData$Version[toolsData$Tool %in% toolName]
+  if (is.na(toolVersion)) {
+    return()
+  }
   toolPath <- switch(toolName,
     "OSP Suite" = paste0(
       "https://github.com/Open-Systems-Pharmacology/OSPSuite-R/releases/download/v",
       toolVersion, "/ospsuite_", toolVersion, ".zip"
+    ),
+    "TLF" = paste0(
+      "https://github.com/Open-Systems-Pharmacology/TLF-Library/releases/download/v",
+      toolVersion, "/tlf_", toolVersion, ".zip"
+    ),
+    "RUtils" = paste0(
+      "https://github.com/Open-Systems-Pharmacology/OSPSuite.RUtils/releases/download/v",
+      toolVersion, "/ospsuite.utils_", toolVersion, ".zip"
+    ),
+    "rClr" = paste0(
+      "https://github.com/Open-Systems-Pharmacology/rClr/releases/download/v",
+      toolVersion, "/rClr_", toolVersion, ".zip"
+    ),
+    "rSharp" = paste0(
+      "https://github.com/Open-Systems-Pharmacology/rSharp/releases/download/v",
+      toolVersion, "/rSharp-v", toolVersion, "-Windows-r_4.4.0.zip"
     ),
     "Qualification Runner" = paste0(
       "https://github.com/Open-Systems-Pharmacology/QualificationRunner/releases/download/v",
@@ -30,36 +55,51 @@ getToolPath <- function(toolName) {
       toolVersion, "/pk-sim-portable-setup.zip"
     )
   )
-  return(toolPath)
+  archiveName <- switch(toolName,
+    "OSP Suite" = "ospsuite",
+    "TLF" = "tlf",
+    "RUtils" = "ospsuite.utils",
+    "rClr" = "rClr",
+    "rSharp" = "rsharp",
+    "Reporting Engine" = "ospsuite.reportingengine",
+    "Qualification Runner" = "qualificationrunner",
+    "PK Sim" = "pk-sim"
+  )
+  toolArchive <- paste0(archiveName, ".zip")
+  # If archive is an R package
+  if (toolName %in% c("OSP Suite", "TLF", "RUtils", "rClr", "rSharp", "Reporting Engine")) {
+    download.file(toolPath, destfile = toolArchive, mode = "wb")
+    if (toolName %in% "rSharp") {
+      unzip(toolArchive)
+      unlink(toolArchive)
+      toolArchive <- list.files(pattern = "rSharp.*\\.zip")
+    }
+    install.packages(toolArchive, repos = NULL, type = "binary")
+    unlink(toolArchive)
+    print(paste(toolName, "version", toolVersion, "installed"))
+    return()
+  }
+  download.file(toolPath, destfile = toolArchive)
+  exdir <- switch(toolName,
+    "Qualification Runner" = "QualificationRunner",
+    "PK Sim" = "PK-Sim"
+  )
+  unzip(toolArchive, exdir = exdir)
+  unlink(toolArchive)
+  file.rename(
+    from = list.files(exdir, full.names = TRUE),
+    to = file.path(exdir, exdir)
+  )
+  print(paste(toolName, "version", toolVersion, "installed"))
+  return()
 }
 
-#' @description Install CRAN R packages from binaries
-#' TODO: this may be changed later on because newer versions of OSPSuite may rely on `{remotes}` package directly
-download.file("https://github.com/Open-Systems-Pharmacology/rSharp/releases/download/v1.0.0/rSharp-v1.0.0-Windows-r_4.4.0.zip", destfile = "rsharp.zip", mode = "wb")
-download.file("https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/ospsuite-rutils/artifacts/ospsuite.utils.zip?pr=false", destfile = "ospsuite.utils.zip", mode = "wb")
-download.file("https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/tlf-library/artifacts/tlf.zip?pr=false", destfile = "tlf.zip", mode = "wb")
-download.file(getToolPath("OSP Suite"), destfile = "ospsuite.zip", mode = "wb")
-download.file(getToolPath("Reporting Engine"), destfile = "ospsuite.reportingengine.zip", mode = "wb")
-unzip("rsharp.zip")
-rSharp_archive <- list.files(pattern = "rSharp.*\\.zip")
-install.packages(rSharp_archive, repos = NULL, type = "binary")
-install.packages("ospsuite.utils.zip", repos = NULL, type = "binary")
-install.packages("tlf.zip", repos = NULL, type = "binary")
-install.packages("ospsuite.zip", repos = NULL, type = "binary")
-install.packages("ospsuite.reportingengine.zip", repos = NULL, type = "binary")
-unlink("rsharp.zip")
-unlink("ospsuite-utils.zip")
-unlink("tlf.zip")
-unlink("ospsuite.zip")
-unlink("ospsuite.reportingengine.zip")
-
-#' @description Setup PK-Sim portable and Qualification Runner released versions
-download.file(getToolPath("Qualification Runner"), destfile = "qualificationrunner.zip")
-unzip("qualificationrunner.zip", exdir = "QualificationRunner")
-unlink("qualificationrunner.zip")
-file.rename(from = list.files("QualificationRunner", full.names = TRUE), to = "QualificationRunner/QualificationRunner")
-
-download.file(getToolPath("PK Sim"), destfile = "pk-sim.zip")
-unzip("pk-sim.zip", exdir = "PK-Sim")
-unlink("pk-sim.zip")
-file.rename(from = list.files("PK-Sim", full.names = TRUE), to = "PK-Sim/PK-Sim")
+toInstall <- c(
+  # R Packages
+  "RUtils", "rClr", "rSharp", "TLF", "OSP Suite", "Reporting Engine", 
+  # Qualification Framework
+  "Qualification Runner", "PK Sim"
+  )
+for(toolName in toInstall){
+  installTool(toolName, toolsData)
+}
